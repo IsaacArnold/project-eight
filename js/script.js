4276b6f9038906f067677045474029a251061821
Isaac Arnold <isaacarnold@icloud.com>
2020-11-25 20:43:15 +1300

    Addes JS to import employee data onto page.
    Also updated styling to ensure cards and grid worked with new data

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ Global variables
 ======================================== */
 
 let employees = [];
-const apiUrl = 'https://randomuser.me/api/?results=12';
+const apiUrl = 'https://randomuser.me/api/?results=12&nat=nz';
+const gridcontainer = document.querySelector('.grid-container');
 
 /* ========================================
 Getting data from API
@@ -12,9 +13,28 @@ Getting data from API
 fetch(apiUrl)
     .then(response => response.json())
     .then(res => res.results)
-    .then (data => console.log(data))
+    .then (data => displayEmployees(data))
     .catch(err => console.log(err));
 
 /* ========================================
 Placing the employee data into the grid
 ======================================== */
+
+function displayEmployees(data) {
+    employees = data;
+    let cardHtml = "";
+
+    employees.forEach((employee, index) => {
+        cardHtml += `
+            <div class='card'>
+                <img src='${employee.picture.large}' alt='Image of ${employee.name.first} ${employee.name.last}' class='profile-img-small'>
+                <div class='text-container'>
+                    <h2 class='name'>${employee.name.first} ${employee.name.last}</h2>
+                    <p class='email'>${employee.email}</p>
+                    <p class='city'>${employee.location.city}</p>
+                </div>
+            </div>
+        `
+    });
+    gridcontainer.innerHTML = cardHtml;
+}
