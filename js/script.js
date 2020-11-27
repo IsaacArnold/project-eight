a18a0994265dc315eaeb2f8c8a8beb6a41faa0b9
Isaac Arnold <isaacarnold@icloud.com>
2020-11-27 14:32:51 +1300

    Added modal layout and JS

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ Global variables
 
 let employees = [];
 const apiUrl = 'https://randomuser.me/api/?results=12&nat=nz';
-const gridcontainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContainer = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
 
 /* ========================================
 Getting data from API
@@ -26,7 +29,7 @@ function displayEmployees(data) {
 
     employees.forEach((employee, index) => {
         cardHtml += `
-            <div class='card'>
+            <div class='card' data-index='${index}'>
                 <img src='${employee.picture.large}' alt='Image of ${employee.name.first} ${employee.name.last}' class='profile-img-small'>
                 <div class='text-container'>
                     <h2 class='name'>${employee.name.first} ${employee.name.last}</h2>
@@ -36,5 +39,56 @@ function displayEmployees(data) {
             </div>
         `
     });
-    gridcontainer.innerHTML = cardHtml;
+    gridContainer.innerHTML = cardHtml;
 }
+
+/* ========================================
+Placing information inside the modal
+======================================== */
+
+function displayModal(index) {
+    let {
+        name, dob, phone, email, location: {city, street, state, postcode}, picture
+    } = employees[index];
+    let date = new Date(dob.date);
+
+
+
+    const modalHtml = `
+        <img class='avatar' src='${picture.large}' alt='Image of ${name.first} ${name.last}'>
+        <div class='text-container'>
+            <h2 class='name'>${name.first} ${name.last}</h2>
+            <p class='email'>${email}</p>
+            <p class='city'>${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class='address'>${street.name}, ${state}, ${postcode}</p>
+            <p>Birthday: ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}<p>
+        </div>
+    `;
+
+    overlay.classList.remove('hidden');
+    modalContainer.innerHTML = modalHtml;
+}
+
+/* ========================================
+Event Listeners
+======================================== */
+
+gridContainer.addEventListener('click', (e) => {
+    // Makes sure the click is not on the gridContainer itself
+    if (e.target !== gridContainer) {
+        // Selects the card element based on its proximity to the actual element clicked
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+// Closes the modal when clicking on the 'X'
+modalClose.addEventListener('click', (e) => {
+    if (e.target.className === 'modal-close') {
+        overlay.classList.add('hidden');
+    }
+});
